39d753beb5c992c20709bdc2f5a10d33edc35cea
matthieu jacquier <matthieu.pro.jacquier@gmail.com>
2025-05-26 15:54:55 +0200

    stupid coaching

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root to: "questions#home"
   # root "posts#index"
+  get "ask", to: "questions#ask"
+  get "answer", to: "questions#answer"
 end
